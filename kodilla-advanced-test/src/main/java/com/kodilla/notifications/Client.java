003f7e88b8670ea8babd37e25479e0b5eca1135a
Marcin Kornowski <kornowski79@gmail.com>
2020-05-26 19:36:05 +0200

    7.6

---
+++ kodilla-advanced-test/src/main/java/com/kodilla/notifications/Client.java
@@ -0,0 +1,5 @@
+package com.kodilla.notifications;
+
+public interface Client {
+    void receive(Notification notification);
+}
---
+++ kodilla-advanced-test/src/main/java/com/kodilla/notifications/Notification.java
@@ -0,0 +1,4 @@
+package com.kodilla.notifications;
+
+public interface Notification {
+}
